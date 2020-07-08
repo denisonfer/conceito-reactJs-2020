596ed2ecda04d9c56041af9c935d8205c7bafad3
Filubil <denison.shenry@gmail.com>
2020-07-07 22:01:18 -0300

    desafio03 realizado para o bootcamp

--- src/App.js
+++ src/App.js
@@ -1,29 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
+  const [repositorios, setRepositorios] = useState([]);
+
+  async function getData() {
+    const { data } = await api.get("repositories");
+
+    setRepositorios(data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const { data } = await api.post("repositories", {
+      title: "Menu 3D animado",
+      url: "https://github.com/denisonfer/menu-3d-animado-rn",
+      techs: ["react-native", "react-native-reanimated"],
+    });
+
+    setRepositorios([...repositorios, data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+    getData();
   }
 
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
+
+      <ul data-testid="repository-list">
+        {repositorios.map((repo) => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
